64d06255a5a684a52356ef0f8528f2fe4f648671
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2022-11-22 21:15:09 -0500

    Adding in base controllers for hello and even-odd cute example

---
+++ src/main/java/com/ebsolutions/controller/EvenOddController.java
@@ -0,0 +1,16 @@
+package com.ebsolutions.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("even-odd")
+public class EvenOddController {
+
+    @GetMapping()
+    public String isNumberPrime(@RequestParam("number") Integer number) {
+        return number % 2 == 0 ? "Even" : "Odd";
+    }
+}
